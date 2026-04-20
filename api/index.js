48ae71e388804569a4759efc9bd0e1b6ef3bc190
Emmanuel Onyeike <emmanuelonyeike33@gmail.com>
2026-04-20 19:00:38 +0100

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,15 @@
+let users = [];
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const { name } = req.body;
+        users.push({ name });
+        return res.status(200).json({ message: "User added" });
+    }
+
+    if (req.method === 'GET') {
+        return res.status(200).json(users);
+    }
+
+    return res.status(405).json({ message: "Method not allowed" });
+}
