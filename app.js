27370fd0e172600f34d3eac00e65a68e2b9d42dc
Emmanuel Onyeike <emmanuelonyeike33@gmail.com>
2026-04-20 19:09:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => {
